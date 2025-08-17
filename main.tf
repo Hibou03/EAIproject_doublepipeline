f0595afe7dc40d1274a9e65c7c8aa8cbf607096b
Hibou03 <HibaElkellali@gmail.com>
2025-08-17 20:56:36 +0100

    dernier changement

--- main.tf
+++ main.tf
@@ -1,3 +1,104 @@
+# terraform {
+#   required_version = ">= 1.5.0"
+
+#   required_providers {
+#     vsphere = {
+#       source  = "hashicorp/vsphere"
+#       version = "2.3.0"
+#     }
+#   }
+# }
+
+# provider "vsphere" {
+#   user                 = var.vsphere_user
+#   password             = var.vsphere_password
+#   vsphere_server       = var.vsphere_server
+#   allow_unverified_ssl = true
+# }
+
+# # Déclaration du datacenter
+# data "vsphere_datacenter" "dc" {
+#   name = var.datacenter
+# }
+
+# # Objet datastore où sera stockée la VM
+# data "vsphere_datastore" "datastore" {
+#   name          = var.datastore
+#   datacenter_id = data.vsphere_datacenter.dc.id
+# }
+
+# # Cluster de calcul (CPU, RAM...)
+# data "vsphere_compute_cluster" "cluster" {
+#   name          = var.cluster
+#   datacenter_id = data.vsphere_datacenter.dc.id
+# }
+
+# # Réseau sur lequel la VM sera connectée
+# data "vsphere_network" "network" {
+#   name          = var.network
+#   datacenter_id = data.vsphere_datacenter.dc.id
+# }
+
+# # Récupérer un template existant pour cloner la VM
+# data "vsphere_virtual_machine" "template" {
+#   name          = var.template
+#   datacenter_id = data.vsphere_datacenter.dc.id
+# }
+
+# # Créer la VM en clonant un template
+# resource "vsphere_virtual_machine" "vm" {
+#   name             = var.vm_name
+#   resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+#   datastore_id     = data.vsphere_datastore.datastore.id
+
+#   num_cpus = var.numcpus
+#   memory   = var.memory
+
+#   tags = [
+#   "owner:hiba",
+#   "environment:dev"
+#   ]
+
+#   guest_id = data.vsphere_virtual_machine.template.guest_id
+
+#   # Config de l'interface réseau
+#   network_interface {
+#     network_id   = data.vsphere_network.network.id
+#     adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
+#   }
+
+#   # Config du disque
+#   disk {
+#     label           = "disk0"
+#     size            = data.vsphere_virtual_machine.template.disks[0].size
+#     eagerly_scrub   = false
+#     thin_provisioned = true
+#   }
+
+#   # Clonage de la VM à partir du template
+#   clone {
+#     template_uuid = data.vsphere_virtual_machine.template.id
+
+#     # Personnalisation du système (nom, IP...)
+#     customize {
+#       linux_options {
+#         host_name = var.vm_name
+#         domain    = "local"
+#       }
+#       network_interface {
+#         ipv4_address = var.ipv4_adress
+#         ipv4_netmask = var.ipv4_netmask
+#       }
+#       ipv4_gateway = var.ipv4_gateway
+#     }
+#   }
+# }
+
+#-------------------------------------------------------------------------------------------------------------
+
+
+
+
 terraform {
   required_version = ">= 1.5.0"
 
@@ -45,19 +146,33 @@ data "vsphere_virtual_machine" "template" {
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
+# resource "vsphere_tag_category" "category" {
+#   name        = "stage"
+#   cardinality = "SINGLE"
+#   description = "Managed by Terraform"
+
+#   associable_types = [
+#     "VirtualMachine"
+#   ]
+# }
+
+# resource "vsphere_tag" "tag" {
+#   name        = "testhiba"
+#   category_id = "${vsphere_tag_category.category.id}"
+#   description = "Managed by Terraform"
+# }
+
 # Créer la VM en clonant un template
 resource "vsphere_virtual_machine" "vm" {
   name             = var.vm_name
   resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
   datastore_id     = data.vsphere_datastore.datastore.id
+  firmware = "efi"
 
   num_cpus = var.numcpus
   memory   = var.memory
 
-  tags = [
-  "owner:hiba",
-  "environment:dev"
-  ]
+  tags = ["testhiba"]
 
   guest_id = data.vsphere_virtual_machine.template.guest_id
 
@@ -83,7 +198,7 @@ resource "vsphere_virtual_machine" "vm" {
     customize {
       linux_options {
         host_name = var.vm_name
-        domain    = "local"
+        domain    = "cloud.eurafric.com"
       }
       network_interface {
         ipv4_address = var.ipv4_adress
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,18 +1,17 @@
-vsphere_user     = "fake-user@vsphere.local"
-vsphere_password = "FakePassword123!"
-vsphere_server   = "192.168.0.100"
+vsphere_user     = "to_bo_defined"
+vsphere_password = "to_be_defined"
+vsphere_server   = "to_be_defined"
 
-datacenter = "Fake-Datacenter"
-datastore  = "Fake-Datastore"
-cluster    = "Fake-Cluster"
-network    = "Fake-Network"
-template   = "Fake-Template-Ubuntu"
-vm_name    = "csbkp-lx-app1"
+datacenter = "to_be_defined"
+datastore  = "to_be_defined"
+cluster    = "to_be_defined"
+network    = "to_be_defined" # segment
+template   = "template-rhel9.2-en-ocpbst-v1"
+vm_name    = "testhiba"
 
-ipv4_adress = "192.168.10.50"
-ipv4_gateway = "192.168.10.1"
+ipv4_adress = "10.241.130.130"
+ipv4_gateway = "10.241.130.1"
+ipv4_netmask = 24
 
-memory = "2048"
-numcpus = "4"
-
-
+memory = "4096"
+numcpus = "2"
